b944b8b8a8c1f461692030cff25b5aaf2dd99b28
Geetendra1 <sengargeetendra123@gmail.com>
2023-06-06 17:14:40 +0530

    added very basic structure of anuglar app

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component,Input } from '@angular/core';
+import { Hero } from '../hero';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  // this hero is coming form parent component, which is heroes component. 
+    @Input() hero?: Hero;
+}
