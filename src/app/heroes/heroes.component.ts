abbd5dea59c6c60ca940e0128eb7a401b37be2e6
Geetendra1 <sengargeetendra123@gmail.com>
2023-06-06 21:06:25 +0530

    added navigation, for details page , dashboard page and list of heros page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 import {MessageService} from '../message.service'
 @Component({
@@ -8,13 +7,13 @@ import {MessageService} from '../message.service'
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
   onSelect(hero:Hero) : void {
-    this.selectedHero = hero
+    // this.selectedHero = hero
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
